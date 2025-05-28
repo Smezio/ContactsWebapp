eef43c677b69e3a34605d1b5bff69c2118c8d405
Smezio <smerilliriccardo@gmail.com>
2025-05-28 10:28:35 +0200

    API: Added DELETE and PUT endpoints for deleting and editing

--- api/src/main/java/com/example/contactswebapp/controller/ContactController.java
+++ api/src/main/java/com/example/contactswebapp/controller/ContactController.java
@@ -1,7 +1,10 @@
 package com.example.contactswebapp.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,6 @@ import com.example.contactswebapp.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -40,6 +42,30 @@ public class ContactController {
         try {
             Contact result = contactService.saveContact(contact);
             return result;
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.getCause());
+        }
+    }
+
+    @PutMapping("/contacts/{id}")
+    public Contact updateContact(@RequestBody Contact contact, @PathVariable("id") Long id) {
+        try {
+            Contact result = contactService.updateContact(contact, id);
+            return result;
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.getCause());
+        }
+    }
+
+    @DeleteMapping("/contacts/{id}")
+    public String deleteContact(@PathVariable("id") Long id) {
+        try {
+            contactService.deleteContactById(id);
+            return "Delete Succeed";
         }
         catch (Exception ex) {
             ex.printStackTrace();
--- api/src/main/java/com/example/contactswebapp/service/ContactService.java
+++ api/src/main/java/com/example/contactswebapp/service/ContactService.java
@@ -5,6 +5,7 @@ import com.example.contactswebapp.repository.ContactRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +25,34 @@ public class ContactService {
         Contact result = contactRepository.save(contact);
         return result;
     };
+
+    public Contact updateContact(Contact contact, Long id) {
+        Optional<Contact> currentContact = contactRepository.findById(id);
+        
+        if(!currentContact.isEmpty()) {
+
+            // Check First Name
+            if(!currentContact.get().getFirstName().equalsIgnoreCase(contact.getFirstName()))
+                currentContact.get().setFirstName(contact.getFirstName());
+
+            // Check Last Name
+            if(!currentContact.get().getLastName().equalsIgnoreCase(contact.getLastName()))
+                currentContact.get().setLastName(contact.getLastName());
+
+            // Check Email
+            if(!currentContact.get().getEmail().equalsIgnoreCase(contact.getEmail()))
+                currentContact.get().setEmail(contact.getEmail());
+
+            // Check Telephone
+            if(!currentContact.get().getTelephone().equalsIgnoreCase(contact.getTelephone()))
+                currentContact.get().setTelephone(contact.getTelephone());
+        }
+
+        Contact result = contactRepository.save(currentContact.get());
+        return result;
+    }
+
+    public void deleteContactById(Long id) {
+        contactRepository.deleteById(id);
+    }
 }
